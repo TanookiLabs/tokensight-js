3d289203571a06c1b87b775b155bd33f3dd95829
Matias Seijas <matiseijas@gmail.com>
2018-06-26 09:36:22 -0400

    WIP

--- src/services/events/events.hooks.js
+++ src/services/events/events.hooks.js
@@ -2,15 +2,16 @@ const onlyInternal = require('../../hooks/only-internal')
 const setEventId = require('../../hooks/set-event-id')
 const scrapeContractFromEvent = require('../../hooks/scrape-contract-from-event')
 const sanitizeAddress = require('../../hooks/sanitize-address')
+const { setNow } = require('feathers-hooks-common')
 
 module.exports = {
   before: {
     all: [],
     find: [],
     get: [],
-    create: [onlyInternal(), setEventId(), sanitizeAddress({ key: 'data.address' })],
-    update: [onlyInternal(), sanitizeAddress({ key: 'data.address' })],
-    patch: [onlyInternal(), sanitizeAddress({ key: 'data.address' })],
+    create: [onlyInternal(), setNow('createdAt', 'updatedAt'), setEventId(), sanitizeAddress({ key: 'data.address' })],
+    update: [onlyInternal(), setNow('updatedAt'), sanitizeAddress({ key: 'data.address' })],
+    patch: [onlyInternal(), setNow('updatedAt'), sanitizeAddress({ key: 'data.address' })],
     remove: [onlyInternal()]
   },
 
